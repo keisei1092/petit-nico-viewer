98706e15aaf3c737b42e6b51cfe2a6eeb57973fa
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-08-11 00:23:35 +0900

    とりあえずそれっぽく表示

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ app.commandLine.appendSwitch('ppapi-flash-version', '22.0.0.209')
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 672,
+    height: 483,
+    frame: true,
+    resizable: true,
+    alwaysOnTop: true,
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
